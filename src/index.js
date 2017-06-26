a5004c511e7fb199d27ab666f484da9e9fe0c7d6
Konrad Kühne <konnykuehne@gmail.com>
2017-06-26 15:20:44 +0200

    adjust to name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import OrMapReplikator from "./node_modules/components/Replikator"
+import ORMapReplikator from "./node_modules/components/Replikator/ORMapReplikator";
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
@@ -9,7 +9,7 @@ const userId = "mail:alice@stechuhr.de";
 const ormapId = "07f6aae2-2b46-4e44-bfd8-058d13977a8a";
 const uri = "ws://localhost:31778";
 
-ReactDOM.render(<OrMapReplikator
+ReactDOM.render(<ORMapReplikator
                   userId={userId}
                   ormapId={ormapId}
                   uri={uri}>
