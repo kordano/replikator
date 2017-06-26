f68d3f1c44a6996f4446b9f92603fd84f4eec242
Konrad Kühne <konnykuehne@gmail.com>
2017-06-26 15:14:42 +0200

    add to demo app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import OrMapReplikator from "./node_modules/components/Replikator"
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const userId = "mail:alice@stechuhr.de";
+const ormapId = "07f6aae2-2b46-4e44-bfd8-058d13977a8a";
+const uri = "ws://localhost:31778";
+
+ReactDOM.render(<OrMapReplikator
+                  userId={userId}
+                  ormapId={ormapId}
+                  uri={uri}>
+                  <App />
+                </OrMapReplikator>,
+                document.getElementById('root'));
+
 registerServiceWorker();
