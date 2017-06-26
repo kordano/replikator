383744526d19ba8772d60f00cca1e90546999b83
Konrad Kühne <konnykuehne@gmail.com>
2017-06-26 16:38:11 +0200

    fix component errors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ORMapReplikator from "./node_modules/components/Replikator/ORMapReplikator";
+import ORMapReplikator from "./node_modules/components/ORMapReplikator/ORMapReplikator.js";
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const userId = "mail:alice@stechuhr.de";
 const ormapId = "07f6aae2-2b46-4e44-bfd8-058d13977a8a";
 const uri = "ws://localhost:31778";
+const evalFunctions = {"add": function(supervisor, old, params) {
+  const transactions = old.transactions;
+  transactions.push(params);
+  return {transactions};
+}}
 
-ReactDOM.render(<ORMapReplikator
-                  userId={userId}
-                  ormapId={ormapId}
-                  uri={uri}>
-                  <App />
-                </OrMapReplikator>,
+ReactDOM.render(<ORMapReplikator userId={userId} ormapId={ormapId} evalFunctions={evalFunctions} uri={uri}><App/></ORMapReplikator>,
                 document.getElementById('root'));
 
 registerServiceWorker();
