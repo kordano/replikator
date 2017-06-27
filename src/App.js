c51903b9a9acbc8d3f2e32e0ab64475819be2008
Konrad Kühne <konnykuehne@gmail.com>
2017-06-27 08:53:54 +0200

    add some transactions to demo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import {associate, hashIt, toEdn} from "replikativ";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.addTransaction = this.addTransaction.bind(this);
+  }
+  addTransaction({description, value, type}) {
+    const tx = {description, value, date: new Date()}
+    const {ormapId, userId, stage} = this.props.replica
+    const hashed = hashIt(toEdn(tx))
+    associate(stage, userId, ormapId, hashed , [["add", tx]]).then(function() {
+      console.log("associated with " + JSON.stringify(tx));
+    }, console.error);
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +27,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={(e) => {this.addTransaction({foo: "bar"})}}>Replicate!</button>
       </div>
     );
   }
